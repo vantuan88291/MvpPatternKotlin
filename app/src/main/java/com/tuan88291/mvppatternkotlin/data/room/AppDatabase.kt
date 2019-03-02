c16b8dcd6f1b3ffd562cd697cb45ee24a64c8e5f
vantuan88291 <vantuan88291@gmail.com>
2019-03-02 19:41:24 +0700

    update room

--- app/src/main/java/com/tuan88291/mvppatternkotlin/data/room/AppDatabase.kt
+++ app/src/main/java/com/tuan88291/mvppatternkotlin/data/room/AppDatabase.kt
@@ -13,16 +13,15 @@ abstract class AppDatabase : RoomDatabase() {
 
     companion object {
 
-        private var INSTANCE: AppDatabase? = null
-
-        fun getAppDatabase(context: Context): AppDatabase {
-            if (INSTANCE == null) {
-                INSTANCE = Room.databaseBuilder(context.applicationContext, AppDatabase::class.java, "user-database")
-                    .build()
+        @Volatile private var INSTANCE: AppDatabase? = null
+        fun getAppDatabase(context: Context): AppDatabase =
+            INSTANCE ?: synchronized(this) {
+                INSTANCE ?: buildDatabase(context).also { INSTANCE = it }
             }
-            return INSTANCE as AppDatabase
-        }
-
+        private fun buildDatabase(context: Context) =
+            Room.databaseBuilder(context.applicationContext,
+                AppDatabase::class.java, "user-database")
+                .build()
         fun destroyInstance() {
             INSTANCE = null
         }
